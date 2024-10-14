acef3d80db690328538d20e73ae59cc5e91fd27f
elodloic <95284344+elodloic@users.noreply.github.com>
2024-10-14 21:16:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# ChatApp
+
+## About the app:
+
+A cross-platform chat application created in React Native. Users will be able to chat with eachother regardless of their mobile device's OS, while enjoying a near-native app performance.
+
+## Technologies Used
+
+- **React Native** (JavaScript framework)
+- **Expo** (Build tool)
+- **Android Studio Emulator** (Testing)
+
+## Setup Instructions
+
+Follow these steps to set up the project locally:
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/elodloic/ChatApp.git
+   ```
+
+2. **Install dependencies**
+
+   ```bash
+   node install
+   ```
+
+3. **Build app**
+
+   ```bash
+   npm run start
+   ```
---
+++ components/Chat.js
@@ -0,0 +1,35 @@
+import { StyleSheet, View, Text } from "react-native";
+import { useEffect } from "react";
+
+const Chat = ({ route, navigation }) => {
+  const { name, selectedColor } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  });
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: selectedColor || "#090C08" }, // Fallback color if none selected
+      ]}
+    >
+      <Text style={styles.text}>Welcome to the chatroom!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
+
+export default Chat;
