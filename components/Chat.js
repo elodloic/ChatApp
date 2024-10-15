269c111d21cc8348af91bfda03e902a3f68f4b49
elodloic <95284344+elodloic@users.noreply.github.com>
2024-10-15 18:00:03 +0200

    Added GiftedChat
    Implemented the GiftedChat library to handle chatting features.

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,92 @@
-import { StyleSheet, View, Text } from "react-native";
-import { useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { useState, useEffect } from "react";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, selectedColor } = route.params;
+  const [messages, setMessages] = useState([]);
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#6D7883", // Sent bubble color
+          },
+          left: {
+            backgroundColor: "#EAEAEA", //Received bubble color
+          },
+        }}
+        textStyle={{
+          right: {
+            color: "#FFFFFF", // Sent text color
+          },
+          left: {
+            color: "#333333", // Received text color
+          },
+        }}
+      />
+    );
+  };
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: `Hi ${name}!`, //Static user message
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "John Doe",
+          avatar: "https://picsum.photos/140/140",
+        },
+      },
+      {
+        _id: 2,
+        text: `${name} has entered the chat.`, //System message
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
-  });
+  }, [name, navigation]);
 
   return (
     <View
       style={[
         styles.container,
-        { backgroundColor: selectedColor || "#090C08" }, // Fallback color if none selected
+        { backgroundColor: selectedColor || "#090C08" }, // Fallback background color if none selected
       ]}
     >
-      <Text style={styles.text}>Welcome to the chatroom!</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" /> // Android keyboard optimization
+      ) : null}
+      {Platform.OS === "ios" ? (
+        <KeyboardAvoidingView behavior="padding" /> // iOS keyboard optimization
+      ) : null}
     </View>
   );
 };
@@ -23,12 +94,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    color: "#fff",
-    fontWeight: "bold",
   },
 });
 
